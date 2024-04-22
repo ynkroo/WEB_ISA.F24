3b4528ea5e118e5dadcb9ea43cbea4b06781da59
mike-debastiani <mike.debastiani@stud.hslu.ch>
2024-04-22 15:44:51 +0200

    test

--- myCampus_Dashboard_Extension_current/popup.js
+++ myCampus_Dashboard_Extension_current/popup.js
@@ -3,7 +3,7 @@ function getECTS() {
 }
 
 function onPage() {
-  // Get Tab and ask to run function for this Tab (idk why its on the Tab)
+  // Get Tab and ask to run function for this Tab
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id },
@@ -44,19 +44,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   }
 });
 
-// Ask for URL -> display diffrent content
+// Always ask for URL and display content accordingly
 document.addEventListener("DOMContentLoaded", function () {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    let url = tabs[0].url;
-    if (
-      url ==
-        "https://mycampus.hslu.ch/de-ch/stud-i/mein-studium/meine-anmeldungen/" ||
-      url ==
-        "https://mycampus.hslu.ch/en/stud-i/mein-studium/meine-anmeldungen/"
-    ) {
-      onPage();
-    } else {
-      notOnPage();
-    }
-  });
+  onPage();
 });
